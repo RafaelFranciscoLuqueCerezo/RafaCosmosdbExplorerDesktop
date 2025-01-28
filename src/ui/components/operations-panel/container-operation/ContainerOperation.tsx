abf3a5fe6eb69752a9a0781e34c2c1a948445c45
Rafael_Cerezo <rafael.luque@servexternos.gruposantander.com>
2025-01-28 14:35:22 +0100

    backup

--- src/ui/components/operations-panel/container-operation/ContainerOperation.tsx
+++ src/ui/components/operations-panel/container-operation/ContainerOperation.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
-import DeleteIcon from '@mui/icons-material/Delete';
+import './ContainerOperation.css';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import PhoneIcon from '@mui/icons-material/Phone';
@@ -15,10 +17,8 @@ export const ContainerOperation = ({operation}:Props)=>{
     const [value, setValue] = useState(0);
     const activeOperation = useAppStore((state)=>state.activeOperation);
     const changeActiveOperation = useAppStore((state)=>state.changeActiveOperation);
-
-    const isActive=()=>{
-        return operation.dbLabel == activeOperation.dbLabel && operation.container == activeOperation.container;
-    }
+    const isActive:boolean = operation.dbLabel == activeOperation.dbLabel && operation.container == activeOperation.container;
+    console.log(isActive);
 
 
     const handleChange = (event: React.SyntheticEvent, newValue: number) => {
@@ -26,12 +26,16 @@ export const ContainerOperation = ({operation}:Props)=>{
     };
 
     return(
-        <div>
-            <div onClick={(_)=>changeActiveOperation(operation)}>
-                <DeleteIcon/>
-                {operation.dbLabel}-{operation.container}
+        <div style={{marginRight:'10px'}}>
+            <div style={{width:'auto'}} onClick={(_)=>changeActiveOperation(operation)}>
+                <div  className={`${isActive && 'container-op-active'}`}style={{padding:'2px',backgroundColor: '#f9f9f9'}}>
+                        <span><strong>{operation.dbLabel}</strong>-{operation.container}</span>
+                        <IconButton aria-label="delete" size="small">
+                            <CloseIcon fontSize="inherit"/>
+                        </IconButton>
+                </div> 
             </div>
-            {isActive() && 
+            {isActive && 
             <Tabs value={value} onChange={handleChange} aria-label="icon tabs example">
                 <Tab icon={<PhoneIcon />} aria-label="phone" />
                 <Tab icon={<FavoriteIcon />} aria-label="favorite" />
